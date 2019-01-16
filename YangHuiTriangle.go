a6b637e610dc8e29ba02a2ff78277abc5694eea1
anntime <893191576@qq.com>
2019-01-16 10:09:41 +0800

    杨辉三角

--- YangHuiTriangle.go
+++ YangHuiTriangle.go
@@ -1,4 +1,40 @@
 package main
 import "fmt"
 //行数
-const LINES 
+const LINES int= 10
+// 杨辉三角
+func ShowYangHuiTriangle(){
+	nums := []int{}
+	for i := 0; i < LINES; i++ {
+		// 补空白
+		for  j:= 0; j < (LINES-i); j++ {
+			fmt.Print(" ")
+		}
+		for index := 0; index < count; index++ {
+			
+		}
+		for < count; indexjgd++ {
+			
+		}
+		for i := 0; i < count; i++ {
+			
+		}
+		for index := 0; index < count; index++ {
+			
+		}
+		for i < count; indexi++ {
+			
+		}
+		for i := 0; index < count; index++ {
+
+		}
+
+
+
+		for index := 0; index < count; index++ {
+			
+		}
+
+
+	}
+}
